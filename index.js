95198e093a0ea3093b1150041c2fe049433918cf
rajdas <mail.rajdas@gmail.com>
2017-07-23 19:01:14 +0530

    scket.io

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
+var express = require('express')
+var app     = express()
+var server  = require('http').createServer(app)
+var io      = require('socket.io').listen(server)
+users = []
+connections = []
 
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+server.listen(process.env.PORT || 3000)
+console.log('server is running');
+app.get('/',function(req,res){
+  res.sendFile(__dirname+'/index.html')
+})
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+io.on('connection',function(socket){
+  connections.push(socket)
+  console.log('Conected: %s sockets connected',connections.length)
+  //disconnect
+  socket.on('disconnect',function(data){
+    users.splice(users.indexOf(socket.username),1)
+    updateUsernames()
+    connections.splice(connections.indexOf(socket),1);
+    console.log('Disconnected : %s sockets connected', connections.length)
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-});
+  })
+  // Send message
+  socket.on('send message',function(data){
+    console.log(data)
+    io.sockets.emit('new message',{msg: data, user: socket.username})
+  })
+  //  New users
+  socket.on('new user', function(data, callback){
+    callback(true)
+    socket.username = data
+    users.push(socket.username)
+    updateUsernames()
+  })
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-  });
-});
-io.emit('some event', { for: 'everyone' });
-io.on('connection', function(socket){
-  socket.broadcast.emit('hi');
-});
-    
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+  function updateUsernames(){
+    io.sockets.emit('get users', users);
+  }
+})
